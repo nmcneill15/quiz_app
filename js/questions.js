712c0369be85bb287920ccb99a617b79f2c69c07
Nathan McNeill <nmcneill@gmail.com>
2016-06-17 09:29:57 -0500

    cleanup

---
+++ js/questions.js
@@ -0,0 +1,48 @@
+var questions = [{
+  question: "Stop! He who would cross the bridge of death must answer me these questions three, ere the other side he see",
+  answers: {"Ask me the questions, bridgekeeper. I am not afraid": true},
+  correct: false,
+  index: 0
+},
+{
+  question: "What is your name?",
+  answers: {"Sir Lancelot of Camelot": true, "Ralph": false, "Brad Pitt": false, "Duke Lancelot of Brooklyn": false},
+  correct: false,
+  index: 1
+},
+{
+  question: "What is your quest?",
+  answers: {"To seek some peace and quiet": false, "To seek the Holy Grail": true, "Two sikhs walk into a bar": false, "Too slick for my own good": false},
+  correct: false,
+  index: 2
+},
+{
+  question: "What is your favorite color?",
+  answers: {"Blue": true, "Purple": false, "Coconut Cream": false, "yellow": false},
+  correct: false,
+  index: 3
+},
+{
+  question: "What is your name?",
+  answers: {"Sir Robin of Camelot": true, "Robin Hood": false, "Robin and Batman": false, "Rob": false},
+  correct: false,
+  index: 4
+},
+{
+  question: "What is your quest?",
+  answers: {"I've been going to counceling to try and answer that": false, "To seek the Holy Grail!": true, "I don't know": false, "To sip pinocolada's on the beach": false},
+  correct: false,
+  index: 5
+},
+{
+  question: "What is the capital of Assyria?",
+  answers: {"I don't know that!": false, "Lithuania": false, "Philadelphia": false, "Ashur": true},
+  correct: false,
+  index: 6
+},
+{
+  question: "What is your name?",
+  answers: {"Arthur, King of the Britons": true, "Ralph...yes, just Ralph": false, "Elvis, King of Rock and Roll": false, "Harry Potter": false},
+  correct: false,
+  index: 7
+}];
