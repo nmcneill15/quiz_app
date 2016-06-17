4a64f9ea36c76b02c2b8140ab753c91e79d5ee1c
Nathan McNeill <nmcneill@gmail.com>
2016-06-17 09:24:44 -0500

    about done

--- js/scripts.js
+++ js/scripts.js
@@ -1,55 +1,56 @@
+
 var questionIndex = 0;
 var deaths = 0;
 var crossings = 0;
 
-  var questions = [{
-    question: "Stop! He who would cross the bridge of death must answer me these questions three, ere the other side he see",
-    answers: {"Ask me the questions, bridgekeeper. I am not afraid": true},
-    correct: false,
-    index: 0
-  },
-  {
-    question: "What is your name?",
-    answers: {"Sir Lancelot of Camelot": true, "Ralph": false, "Brad Pitt": false, "Duke Lancelot of Brooklyn": false},
-    correct: false,
-    index: 1
-  },
-  {
-    question: "What is your quest?",
-    answers: {"To seek some peace and quiet": false, "To seek the Holy Grail": true, "Two sikhs walk into a bar": false, "Too slick for my own good": false},
-    correct: false,
-    index: 2
-  },
-  {
-    question: "What is your favorite color?",
-    answers: {"Blue": true, "Purple": false, "Coconut Cream": false, "yellow": false},
-    correct: false,
-    index: 3
-  },
-  {
-    question: "What is your name?",
-    answers: {"Sir Robin of Camelot": true, "Robin Hood": false, "Robin and Batman": false, "Rob": false},
-    correct: false,
-    index: 4
-  },
-  {
-    question: "What is your quest?",
-    answers: {"I've been going to counceling to try and answer that": false, "To seek the Holy Grail!": true, "I don't know": false, "To sip pinocolada's on the beach": false},
-    correct: false,
-    index: 5
-  },
-  {
-    question: "What is the capital of Assyria?",
-    answers: {"I don't know that!": false, "Lithuania": false, "Philadelphia": false, "Ashur": true},
-    correct: false,
-    index: 6
-  },
-  {
-    question: "What is your name?",
-    answers: {"Arthur, King of the Britons": true, "Ralph...yes, just Ralph": false, "Elvis, King of Rock and Roll": false, "Harry Potter": false},
-    correct: false,
-    index: 7
-  }];
+var questions = [{
+  question: "Stop! He who would cross the bridge of death must answer me these questions three, ere the other side he see",
+  answers: {"Ask me the questions, bridgekeeper. I am not afraid": true},
+},
+{
+  question: "What is your name?",
+  answers: {"Sir Lancelot of Camelot": true, "Ralph": false, "Brad Pitt": false, "Duke Lancelot of Brooklyn": false},
+},
+{
+  question: "What is your quest?",
+  answers: {"To seek some peace and quiet": false, "To seek the Holy Grail": true, "Two sikhs walk into a bar": false, "Too slick for my own good": false},
+},
+{
+  question: "What is your favorite color?",
+  answers: {"Blue": true, "Purple": false, "Coconut Cream": false, "yellow": false},
+},
+{
+  question: "What is your name?",
+  answers: {"Sir Robin of Camelot": true, "Robin Hood": false, "Robin and Batman": false, "Rob": false},
+},
+{
+  question: "What is your quest?",
+  answers: {"I've been going to counceling to try and answer that": false, "To seek the Holy Grail!": true, "I don't know": false, "To sip pinocolada's on the beach": false},
+},
+{
+  question: "What is the capital of Assyria?",
+  answers: {"I don't know that!": false, "Lithuania": false, "Philadelphia": false, "Ashur": true},
+},
+{
+  question: "What...is your name?",
+  answers: {"Arthur, King of the Britons": true, "Ralph...yes, just Ralph": false, "Elvis, King of Rock and Roll": false, "Harry Potter": false},
+},
+{
+  question: "What...is your quest?",
+  answers: {"I dunno": true, "To seek the Holy Grail!": true, "To seek discounts after Christmas": false, "Could you repeat the question": false},
+},
+{
+  question: "What...is the airspeed velocity of an unladen swallow?",
+  answers: {"I don't know that!": false, "42": false, "What do you mean, an African or European swallow?": true, "about 23 mph": false},
+},
+{
+  question: "How do you know so much about swallows?",
+  answers: {"Well, you have to know these sorts of things when you're a king, you know": true},
+}
+
+];
+
+
 
 $(document).ready(function() {
   newQuiz();
@@ -61,18 +62,28 @@ function isCorrect(answer) {
   return correct;
 }
 function correct() {
-  $("#crossing-modal").fadeIn().delay(200).fadeOut();
+  if (questionIndex !== questions.length - 1) {
+    $("#qa").hide();
+    $("#crossing-modal").fadeIn().delay(300).fadeOut(function() {
+      $("#qa").show();
+    });
+  }
   $("#crossings").append("<img src='img/small-bridge.png' />");
   offYouGo = document.getElementById("off-you-go-audio");
   offYouGo.play();
   crossings++;
+
 }
 
 function incorrect() {
-  $("#deaths-modal").append("<img class='die' src='img/die.gif' />");
-  $("#deaths-modal").fadeIn().delay(200).fadeOut(function(){
-    $("#deaths-modal").empty();
-  });
+  if (questionIndex !== questions.length - 1) {
+    $("#qa").hide();
+    $("#deaths-modal").append("<img class='die' src='img/die.gif' />");
+    $("#deaths-modal").fadeIn().delay(3000).fadeOut(function(){
+      $("#deaths-modal").empty();
+      $("#qa").show();
+    });
+  }
   $("#deaths").append("<img src='img/die.png' />");
   dontKnow = document.getElementById("dont-know-audio");
   dontKnow.currentTime = 12;
@@ -87,9 +98,6 @@ $("#answers").on("click", ".answer-btn", function(e) {
   if (right === true) {
     if (questionIndex > 0) {
       correct();
-    } else {
-      notAfraidAudio = document.getElementById("not-afraid-audio");
-      notAfraidAudio.play();
     }
     nextQuestion();
   } else {
@@ -97,6 +105,13 @@ $("#answers").on("click", ".answer-btn", function(e) {
       incorrect();
     }
     nextQuestion();
+  }
+});
+
+$("#answers").on("mouseover", function() {
+  if (questionIndex === 0) {
+    notAfraidAudio = document.getElementById("not-afraid-audio");
+    notAfraidAudio.play();
   }
 });
 
